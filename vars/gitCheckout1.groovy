56ed6afc2eaa260e533bd9a66ba11a73e7d390b9
kdhani <37719348+kdhani@users.noreply.github.com>
2024-02-16 14:01:56 +0530

    Update gitCheckout1.groovy

--- vars/gitCheckout1.groovy
+++ vars/gitCheckout1.groovy
@@ -2,7 +2,7 @@ def call(Map stageParams) {
  
     checkout([
         $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
+        branches: [[name1:  stageParams.branch ]],
         urls: [[ url: stageParams.url ]]
     ])
   }
